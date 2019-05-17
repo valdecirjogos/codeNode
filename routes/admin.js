f113a302f7f077ab57bc52e9366be310391f9599
valdecir <valdecir.neper@gmail.com>
2019-05-17 10:39:19 -0300

    adding some data just to see

--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,7 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const product = [];
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
@@ -13,8 +14,11 @@ router.get('/add-product', (req, res, next) => {
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
+  //console.log(req.body);
+  product.push({title: req.body.title});
   res.redirect('/');
 });
 
-module.exports = router;
+//module.exports = router;
+exports.routers = router;
+exports.product = product;
